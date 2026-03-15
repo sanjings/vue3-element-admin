d34b96435db4c9c7a8ad1f24403d1b107706127e
sanjings <sanjings@qq.com>
2026-03-15 23:50:07 +0800

    perf: ts工具类型优化

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,13 +1,12 @@
-declare type Nullable<T> = T | null;
 declare type Recordable<T = any> = Record<string, T>;
 declare type ReadonlyRecordable<T = any> = Readonly<Record<string, T>>;
 
 declare type TimeoutHandle = ReturnType<typeof setTimeout>;
 declare type IntervalHandle = ReturnType<typeof setInterval>;
 
-declare type RequireKey<T, K extends keyof T> = Omit<T, K> & {
-  [P in K]-?: T[P];
-};
+declare type GetArrayItem<T> = NonNullable<T>[number];
+
+declare type RequireKey<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
 
 declare interface Window {
   _AMapSecurityConfig: {
